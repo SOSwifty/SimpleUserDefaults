b815ffd94c20f609a4c6c9f35a20d748d940d71e
Snir Orlanczyk <sorlanczyk@chegg.com>
2022-06-17 12:55:10 -0400

    add remove volition for test

--- Tests/SimpleUserDefaultsTests/UserDefaultsStorageTests.swift
+++ Tests/SimpleUserDefaultsTests/UserDefaultsStorageTests.swift
@@ -5,6 +5,7 @@ extension UserDefaults {
     static func resetDefaults() {
         if let bundleID = Bundle.main.bundleIdentifier {
             UserDefaults.standard.removePersistentDomain(forName: bundleID)
+            UserDefaults.standard.removeVolatileDomain(forName: bundleID)
         }
     }
 }
